559123bf8d2f86d2b7b5a20164341a99f0c57775
pcm9909 <pcm9909@naver.com>
2024-08-31 21:26:25 +0900

    [fix] parse command

--- main.c
+++ main.c
@@ -447,7 +447,7 @@ void parse_right_redirection(char *str, int *i, t_redirection *command)
     free(content);
 }
 
-void parse_command(char *str, int *i, t_redirection *command)
+void parse_command(char *str, int *i, t_redirection *command, char **envp)
 {
     int j;
     char *content;
@@ -478,6 +478,8 @@ void parse_command(char *str, int *i, t_redirection *command)
         free(content);
     }
 }
+
+
 
 char *ft_find_single_redirect(char *str, char c)
 {
@@ -564,15 +566,67 @@ char *set_command(t_command *command)
     return result;
 }
 
+void parse_command_fix(char *str, int *i, t_redirection *command, char **envp)
+{
+    int j;
+    char *temp;
+    char *content;
+    int env_idx;
 
-void parse_redirection(char *str, t_redirection *command)
+    content = ft_strdup("");
+	while(str[*i] && str[*i] != ' ')
+	{
+		if (str[*i] == '"')
+		{
+			(*i)++;
+			j = (*i);
+			while (str[*i] && str[*i] != '"' && str[*i] != '$')
+				(*i)++;
+			temp = ft_substr(str, j, *i - j);
+			(*i)++;
+		}
+		else if (str[*i] == '\'')
+		{
+			(*i)++;
+			j = (*i);
+			while (str[*i] && str[*i] != '\'')
+				(*i)++;
+			temp = ft_substr(str, j, *i - j);
+			(*i)++;
+		}
+		else if (str[*i] == '$')
+		{
+			(*i)++;
+			j = (*i);
+			while (str[*i] && str[*i] != '\'' && str[*i] != '"')
+				(*i)++;
+			char *env_var = ft_substr(str, j, *i - j);
+			temp = ft_strdup(envp[search_env(envp, env_var, 1)]);
+			free(env_var);
+		}
+		else
+		{
+			j = (*i);
+			while (str[*i] && str[*i] != '\'' && str[*i] != '"' && str[*i] != ' ' && str[*i] != '$')
+				(*i)++;
+			temp = ft_substr(str, j, *i - j);
+		}
+		char *new_content = ft_strjoin_with_free(content, temp);
+		free(temp);
+		content = new_content;
+	}
+    command->command->command = append_command(&command->command->command, content);
+    free(content);
+}
+
+void parse_redirection(char *str, t_redirection *command, char **envp)
 {
     int i = 0;
 
     while (str[i])
     {
         while (is_whitespace(str[i]))
-            i++;
+			i++;
         if (str[i] == '<')
         {
             parse_left_redirection(str, &i, command);
@@ -583,7 +637,7 @@ void parse_redirection(char *str, t_redirection *command)
         }
         else
         {
-            parse_command(str, &i, command);
+            parse_command_fix(str, &i, command, envp);
         }
     }
 	command->full_cmd = set_command(command->command);
@@ -677,12 +731,13 @@ void process_input(char *str, char **envp)
     for (int i = 0; i < cnt; i++)
     {
         initialize_redirection(&command[i]);
-        parse_redirection(split[i], command[i]);
+        parse_redirection(split[i], command[i], envp);
     }
 
     for (int i = 0; i < cnt; i++)
     {
-        if (i < cnt - 1) {
+        if (i < cnt - 1)
+		{
             if (pipe(pipe_fd) == -1)
             {
                 perror("pipe");
