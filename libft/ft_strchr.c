9b003f9e673440a380c59dadb034717ba8521d34
pcm9909 <pcm9909@naver.com>
2024-09-19 22:47:26 +0900

    [fix] add when input char * doesn't exist

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,13 +6,15 @@
 /*   By: chunpark <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 11:22:03 by chunpark          #+#    #+#             */
-/*   Updated: 2024/02/29 11:22:26 by chunpark         ###   ########.fr       */
+/*   Updated: 2024/09/19 22:46:54 by chunpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == c)
