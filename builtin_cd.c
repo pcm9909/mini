493b3d31eb16d8870e40194faf351ac99d8c3ff8
Dendrog <goodshs335@gmail.com>
2024-10-19 21:33:55 +0900

    norm

--- builtin_cd.c
+++ builtin_cd.c
@@ -1,74 +1,94 @@
 #include "main.h"
 
-void handle_cd_command(t_redir *command, char ***envp)
+void	dir_err(char **cd, char ***envp)
 {
-    char *tmp_pwd;
-    char **cd;
-    char **cd_path;
-    char *old;
-    int i;
+	ft_putstr_fd("minishell: cd: ", 2);
+	ft_putstr_fd(cd[1], 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
+	set_dollar(1, envp);
+}
 
-    tmp_pwd = getcwd(NULL, BUFSIZ);
-    old = getcwd(NULL, BUFSIZ);
-    cd = ft_strdups(command->cmd->cmd_val);
-    if (cd[1] == NULL)
-    {
-        free(tmp_pwd);
-        tmp_pwd = ft_strdup(extract_home(*envp));
-    }
-    else if (cd[2] != NULL)
-    {
-        ft_putstr_fd("minishell: cd: too many arguments\n", 2);
-        set_dollar(1, envp);
-    }
-    else
-    {
-        if (!ft_strncmp(cd[1], "/", 1))
-        {
-            free(tmp_pwd);
-            tmp_pwd = ft_strdup("/");
-        }
-        i = 0;
-        cd_path = ft_split(cd[1], '/');
-        while (cd_path[i] != NULL)
-        {
-            if (i == 0 && !ft_strncmp(cd_path[i], "~", 4))
-            {
-                free(tmp_pwd);
-                tmp_pwd = ft_strdup(extract_home(*envp));
-            }
-            else
-            {
-                if (!ft_strncmp(cd_path[i], "..", 5))
-                {
-                    if (ft_strrchr(tmp_pwd, '/') == tmp_pwd)
-                        *(ft_strrchr(tmp_pwd, '/') + 1) = '\0';
-                    else
-                        *(ft_strrchr(tmp_pwd, '/')) = '\0';
-                }
-                else
-                {
-                    tmp_pwd = ft_strjoin_opts(tmp_pwd, "/", 1);
-                    tmp_pwd = ft_strjoin_opts(tmp_pwd, cd_path[i], 1);
-                }
-            }
-            i++;
-        }
-        all_free(cd_path);
-    }
-    if (chdir(tmp_pwd) == -1)
-    {
-        ft_putstr_fd("minishell: cd: ", 2);
-        ft_putstr_fd(cd[1], 2);
-        ft_putstr_fd(": No such file or directory\n", 2);
-        set_dollar(1, envp);
-    }
-    else
-    {
-        set_env(ft_strjoin("OLDPWD=", old), 1, envp);
-        set_env(ft_strjoin("PWD=", tmp_pwd), 1, envp);
-    }
-    free(old);
-    free(tmp_pwd);
-    all_free(cd);
+void	dir_move(char *old, char *tmp_pwd, char ***envp, int flag)
+{
+	if (flag == 1)
+	{
+		set_env(ft_strjoin("OLDPWD=", old), 1, envp);
+		set_env(ft_strjoin("PWD=", tmp_pwd), 1, envp);
+	}
+	else
+	{
+		free(old);
+		free(tmp_pwd);
+	}
+}
+
+void	make_cd_path(char **cd_path, char **tmp_pwd, int i)
+{
+	if (!ft_strncmp(cd_path[i], "..", 5))
+	{
+		if (ft_strrchr(*tmp_pwd, '/') == *tmp_pwd)
+			*(ft_strrchr(*tmp_pwd, '/') + 1) = '\0';
+		else
+			*(ft_strrchr(*tmp_pwd, '/')) = '\0';
+	}
+	else
+	{
+		*tmp_pwd = ft_strjoin_opts(*tmp_pwd, "/", 1);
+		*tmp_pwd = ft_strjoin_opts(*tmp_pwd, cd_path[i], 1);
+	}
+}
+
+void	cd_check(char **cd, char **tmp_pwd, char ***envp)
+{
+	char	**cd_path;
+	int		i;
+
+	if (!ft_strncmp(cd[1], "/", 1))
+	{
+		free(*tmp_pwd);
+		*tmp_pwd = ft_strdup("/");
+	}
+	i = 0;
+	cd_path = ft_split(cd[1], '/');
+	while (cd_path[i] != NULL)
+	{
+		if (i == 0 && !ft_strncmp(cd_path[i], "~", 4))
+		{
+			free(*tmp_pwd);
+			*tmp_pwd = ft_strdup(extract_home(*envp));
+		}
+		else
+			make_cd_path(cd_path, tmp_pwd, i);
+		i++;
+	}
+	all_free(cd_path);
+}
+
+void	handle_cd_command(t_redir *command, char ***envp)
+{
+	char	*tmp_pwd;
+	char	**cd;
+	char	*old;
+
+	tmp_pwd = getcwd(NULL, BUFSIZ);
+	old = getcwd(NULL, BUFSIZ);
+	cd = ft_strdups(command->cmd->cmd_val);
+	if (cd[1] == NULL)
+	{
+		free(tmp_pwd);
+		tmp_pwd = ft_strdup(extract_home(*envp));
+	}
+	else if (cd[2] != NULL)
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
+		set_dollar(1, envp);
+	}
+	else
+		cd_check(cd, &tmp_pwd, envp);
+	if (chdir(tmp_pwd) == -1)
+		dir_err(cd, envp);
+	else
+		dir_move(old, tmp_pwd, envp, 1);
+	dir_move(old, tmp_pwd, envp, 0);
+	all_free(cd);
 }
--- builtin_export.c
+++ builtin_export.c
@@ -1,42 +1,42 @@
 #include "main.h"
 
-static int ft_export(char **ptr, char ***envp)
+static int	ft_export(char **ptr, char ***envp)
 {
-    int i;
-    int flag;
-    char *rax;
-    char *tmp;
+	int		i;
+	int		flag;
+	char	*rax;
+	char	*tmp;
 
-    i = 1;
-    flag = 0;
-    while (ptr[i] != NULL)
-    {
-        tmp = ft_strdup(ptr[i]);
-        rax = set_env(tmp, env_validate(ptr[i]), envp);
-        if (flag == 0 && rax != NULL)
-        {
-            ft_putstr_fd("minishell: export: `", 2);
-            ft_putstr_fd(rax, 2);
-            ft_putstr_fd("': not a valid identifier\n", 2);
-            flag = 1;
-        }
-        if (rax != NULL)
-            free(tmp);
-        i++;
-    }
-    return (flag);
+	i = 1;
+	flag = 0;
+	while (ptr[i] != NULL)
+	{
+		tmp = ft_strdup(ptr[i]);
+		rax = set_env(tmp, env_validate(ptr[i]), envp);
+		if (flag == 0 && rax != NULL)
+		{
+			ft_putstr_fd("minishell: export: `", 2);
+			ft_putstr_fd(rax, 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			flag = 1;
+		}
+		if (rax != NULL)
+			free(tmp);
+		i++;
+	}
+	return (flag);
 }
 
-void handle_export_command(t_redir *command, char ***envp)
+void	handle_export_command(t_redir *command, char ***envp)
 {
-    char **cd;
+	char	**cd;
 
-    cd = ft_strdups(command->cmd->cmd_val);
-    if (cd[1] == NULL)
-    {
-        print_envp(*envp, 1);
-    }
-    else
-        set_dollar(ft_export(cd, envp), envp);
-    all_free(cd);
+	cd = ft_strdups(command->cmd->cmd_val);
+	if (cd[1] == NULL)
+	{
+		print_envp(*envp, 1);
+	}
+	else
+		set_dollar(ft_export(cd, envp), envp);
+	all_free(cd);
 }
