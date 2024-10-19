fdeb15bb69458ce3c3e959c2eecb4916870631e1
pcm9909 <pcm9909@naver.com>
2024-10-20 04:49:46 +0900

    [Refactor] norm 파일 분리

--- builtin_utils1.c
+++ builtin_utils1.c
@@ -1,71 +1,5 @@
 #include "main.h"
 
-void	set_dollar(int ptr, char ***envp)
-{
-	char	*tmp;
-	char	*jo;
-
-	tmp = ft_itoa(ptr);
-	jo = ft_strjoin_opts("?=", tmp, 2);
-	set_env(jo, 1, envp);
-}
-
-char	**update_envp(char **envp, int type, char *new)
-{
-	char	**ptr;
-	int		i;
-
-	i = 0;
-	ptr = envp;
-	while (*ptr != NULL)
-	{
-		i++;
-		ptr++;
-	}
-	ptr = (char **)malloc((i + 5) * sizeof(char *));
-	i = 0;
-	while (envp[i] != NULL)
-	{
-		ptr[i] = ft_strdup(envp[i]);
-		i++;
-	}
-	ptr[i++] = new;
-	ptr[i] = NULL;
-	if (type == 1)
-		all_free(envp);
-	return (ptr);
-}
-
-int	search_env(char **envp, char *name, int flag)
-{
-	char	*re;
-	char	*tmp;
-	int		i;
-
-	i = 0;
-	if (ft_strchr(name, '=') && flag == 1)
-		re = ft_substr(name, 0, ft_strchr(name, '=') - name);
-	else
-		re = ft_strdup(name);
-	while (*envp)
-	{
-		if (ft_strchr(*envp, '='))
-			tmp = ft_substr(*envp, 0, ft_strchr(*envp, '=') - *envp);
-		else
-			tmp = ft_strdup(*envp);
-		if (!ft_strncmp(tmp, re, ft_strlen(re) + 1))
-		{
-			free(tmp);
-			break ;
-		}
-		envp++;
-		i++;
-		free(tmp);
-	}
-	free(re);
-	return (i);
-}
-
 static int	is_validname(char *ptr)
 {
 	while (*ptr && *ptr != '=')
@@ -80,36 +14,20 @@ static int	is_validname(char *ptr)
 int	env_validate(char *ptr)
 {
 	char	*dup_ptr;
+	int		ret;
 
 	dup_ptr = ft_strdup(ptr);
 	if ((ft_isalpha(*dup_ptr) || *dup_ptr == '_') && is_validname(dup_ptr))
 	{
 		if (ft_strchr(dup_ptr, '='))
-		{
-			if ((ft_strchr(dup_ptr, '=') + 1)[0] == '\0')
-			{
-				free(dup_ptr);
-				return (1);
-			}
-			if (ft_strchr(dup_ptr, '=') != ft_strrchr(dup_ptr, '='))
-			{
-				free(dup_ptr);
-				return (1);
-			}
-			free(dup_ptr);
-			return (1);
-		}
+			ret = 1;
 		else
-		{
-			free(dup_ptr);
-			return (0);
-		}
+			ret = 0;
 	}
 	else
-	{
-		free(dup_ptr);
-		return (-1);
-	}
+		ret = -1;
+	free(dup_ptr);
+	return (ret);
 }
 
 char	*extract_home(char *envp[])
